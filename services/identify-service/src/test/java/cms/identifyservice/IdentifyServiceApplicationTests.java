682cffb6a48a7429bf8796d9d6fcc84b96e89793
XuanToan01-hn <xuantoan10022002@gmail.com>
2026-07-24 16:59:17 +0700

    chore: initialize CourseHub backend project

---
+++ services/identify-service/src/test/java/cms/identifyservice/IdentifyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cms.identifyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentifyServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ services/payment-service/src/test/java/cms/paymentservice/PaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cms.paymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
