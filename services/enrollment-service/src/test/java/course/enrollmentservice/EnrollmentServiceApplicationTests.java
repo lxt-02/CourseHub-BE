38d57410401894353ac20f4deeb005e3ede02097
XuanToan01-hn <xuantoan10022002@gmail.com>
2026-08-25 10:48:48 +0700

    create sd code bÃase

---
+++ services/enrollment-service/src/test/java/course/enrollmentservice/EnrollmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package course.enrollmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnrollmentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ services/learning-service/src/test/java/course/learningservice/LearningServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package course.learningservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
